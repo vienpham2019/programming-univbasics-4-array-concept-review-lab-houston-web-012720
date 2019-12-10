9021be1548960508a6d4e2b78eaa4bcd451237f6
vien pham <vienpham2019@gmail.com>
2019-12-10 01:20:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
   # Add your solution here
+  newArray = array.sort
+  return newArray[-1]
 end
 
 def find_min_value(array)
   # Add your solution here
+  newArray = array.sort
+  return newArray[0]
 end
